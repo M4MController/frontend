e52ffe5dc6af7c2df98fa832674b357653812e3a
Ed Asriyan <ed-asriyan@protonmail.com>
2018-08-19 19:01:43 +0300

    Lint handlebars templates #7

---
+++ .template-lintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'recommended',
+
+  rules: {
+    'no-trailing-dot-in-path-expression': true,
+    'no-duplicate-attributes': true,
+    'no-inline-styles': true,
+    'no-trailing-spaces': true,
+    'no-outlet-outside-routes': true,
+    'quotes': 'double',
+  },
+};
