1f5193b16ccccc3ea9621fc422f5d933a04a89a4
Ed Asriyan <ed-asriyan@protonmail.com>
2018-08-22 15:08:25 +0300

    Add rule for clickable cards #5

--- app/components/sensor-card.js
+++ app/components/sensor-card.js
@@ -6,6 +6,7 @@ export default class extends Component {
     this.get('styleNamespace'), /* eslint-disable-line no-invalid-this */
     'item',
     'card',
+    'clickable',
     'col-4',
     'no-padding',
   ];
