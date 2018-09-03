e4f8f517ef54f183e780c513057ea7fe96e3271b
--raplace-all <ed-asriyan@protonmail.com>
2018-09-03 17:33:13 +0300

    Create change-password card #14

---
+++ app/components/cards/settings-change-password.js
@@ -0,0 +1,34 @@
+import Component from '@ember/component';
+import {
+  action,
+  computed,
+} from '@ember-decorators/object';
+
+const MIN_PASSWORD_LENGTH = 4;
+
+export default class extends Component {
+  currentPassword = '';
+  newPassword = '';
+  newPasswordAgain = '';
+
+  @computed('currentPassword', 'newPassword', 'newPasswordAgain')
+  get canSave() {
+    const currentNotEmpty = this.get('currentPassword.length');
+    const lengthCheck = this.get('newPassword.length') >= MIN_PASSWORD_LENGTH;
+    const areEqual = this.get('newPassword') === this.get('newPasswordAgain');
+
+    return currentNotEmpty && lengthCheck && areEqual;
+  }
+
+  @action
+  saveBtnClick() {
+    if (!this.attrs.onSave || !this.get('canSave')) return;
+
+    if (!this.attrs.onSave(this.get('newPassword'))) {
+      // if successfully saved
+      this.set('currentPassword', '');
+      this.set('newPassword', '');
+      this.set('newPasswordAgain', '');
+    }
+  }
+}
---
+++ app/controllers/account/settings.js
@@ -0,0 +1,8 @@
+import Controller from '@ember/controller';
+import {action} from '@ember-decorators/object';
+
+export default class extends Controller {
+  @action
+  updatePasswordAction() {
+  }
+}
