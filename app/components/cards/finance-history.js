95660b63750471893bccb4c2429f0226e008e7b6
Yaxo-O <aaazzzx@mail.ru>
2018-09-07 23:34:04 +0100

    FinanceHistory: finance history card#2

--- app/components/cards/finance-history.js
+++ app/components/cards/finance-history.js
@@ -1,8 +1,42 @@
 import Component from '@ember/component';
+import {computed} from '@ember-decorators/object';
 
 export default class extends Component {
-  exampleValues = [
-    {text: 'some'},
-    {text: 'value'},
+  pay_history = [
+    {time: '1 апреля 23:36', expenses: [
+        {provider: 'Ростелеком', service: 'Интернет', cost: 1000},
+        {provider: 'ЖКХ', service: 'Горячая вода', cost: 600},
+        {provider: 'ЖКХ', service: 'Холодная вода', cost: 500},
+      ], @computed('expenses')
+    get total() {
+        return this.expenses.reduce((prev, cur) => prev + cur.cost, 0);
+      }},
+    {time: '1 апреля 23:36', expenses: [
+        {provider: 'Ростелеком', service: 'Интернет', cost: 1000},
+        {provider: 'ЖКХ', service: 'Горячая вода', cost: 600},
+        {provider: 'ЖКХ', service: 'Холодная вода', cost: 500},
+      ], @computed('expenses')
+      get total() {
+        return this.expenses.reduce((prev, cur) => prev + cur.cost, 0);
+      }},
+    {time: '1 апреля 23:36', expenses: [
+        {provider: 'Ростелеком', service: 'Интернет', cost: 1000},
+        {provider: 'ЖКХ', service: 'Горячая вода', cost: 600},
+        {provider: 'ЖКХ', service: 'Холодная вода', cost: 500},
+      ], @computed('expenses')
+      get total() {
+        return this.expenses.reduce((prev, cur) => prev + cur.cost, 0);
+      }},
   ];
+
+  start = new Date(2017, 4, 1);
+  end = new Date(2017, 4, 11);
+
+  @computed('start', 'end')
+  get formatedRange() {
+    let formatNumber = (num) => num < 10 ? '0' + num : '' + num;
+    let format = (date) => formatNumber(date.getDay()) + '.' + formatNumber(date.getMonth()) + '.' + date.getFullYear();
+
+    return format(this.get('start')) + ' - ' + format(this.get('end'));
+  }
 }
