bbc4e8a90cfcd6933c626c3a7f4939a0274ec07b
Ed Asriyan <ed-asriyan@protonmail.com>
2018-10-07 21:17:38 +0300

    Create site-body component #25

--- app/overrides/component.js
+++ app/overrides/component.js
@@ -1,18 +1,33 @@
-import Component from '@ember/component';
-import {get, set} from '@ember/object';
+import Ember from 'ember';
+import {action, computed} from '@ember-decorators/object';
+import {className} from '@ember-decorators/component';
 
-Component.reopen({
-  actions: {
-    toggle(obj, property) {
-      if (!property) {
-        property = obj;
-        obj = this;
-      }
-      set(obj, property, !get(obj, property));
-    },
+Ember.Component = class extends Ember.Component {
+  @computed('localClassNames', 'styleNamespace')
+  @className
+  get _localClassNames() {
+    const localClassNames = this.get('localClassNames');
+    if (localClassNames) {
+      const styleNamespace = this.get('styleNamespace');
+      return localClassNames.map((x) => `${styleNamespace}${x}`).
+        join(' ');
+    } else {
+      return '';
+    }
+  }
 
-    transitionToRoute(route, ...args) {
-      this.get('router').transitionTo(route, ...(args.slice(0, args.length - 1)));
-    },
-  },
-});
+  @action
+  toggle(obj, property) {
+    if (!property) {
+      property = obj;
+      obj = this;
+    }
+    this.set(obj, property, !this.get(obj, property));
+  }
+
+  @action
+  transitionToRoute(route, ...args) {
+    this.get('router').
+      transitionTo(route, ...(args.slice(0, args.length - 1)));
+  }
+};
