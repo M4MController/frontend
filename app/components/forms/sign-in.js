6b164cf1ae67527336e861d66473d4e467539ce3
Ed Asriyan <ed-asriyan@protonmail.com>
2018-10-27 19:16:41 +0300

    Attach existing login page to the new web application #35

---
+++ app/components/forms/sign-in.js
@@ -0,0 +1,58 @@
+import Component from '@ember/component';
+import {
+  action,
+  computed,
+} from '@ember-decorators/object';
+
+const minPasswordLength = 4;
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export default class extends Component {
+  username = '';
+  password = '';
+
+  wasValidated = false;
+
+  @computed('username')
+  get usernameError() {
+    const username = this.get('username');
+
+    if (username.length === 0) {
+      return 'Введите электронную почту';
+    }
+
+    if (!emailRegex.test(username)) {
+      return 'Это не адрес электронной почты';
+    }
+
+    return undefined;
+  }
+
+  @computed('password')
+  get passwordError() {
+    const password = this.get('password');
+
+    if (password.length === 0) {
+      return 'Введите пароль';
+    }
+
+    if (password.length < minPasswordLength) {
+      return `Пароль не может быть короче ${minPasswordLength} символов`;
+    }
+
+    return undefined;
+  }
+
+  @computed('usernameError', 'passwordError')
+  get isValid() {
+    return this.get('usernameError') === undefined &&
+      this.get('passwordError') === undefined;
+  }
+
+  @action
+  onLoginClickAction() {
+    this.set('wasValidated', true);
+    this.get('isValid') && this.attrs.onLogIn &&
+    this.attrs.onLogIn(this.get('username'), this.get('password'));
+  }
+}
