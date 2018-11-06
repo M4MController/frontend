08c27929f20ee3d22176c96c71025454c7ebfc68
Yaxo-O <aaazzzx@mail.ru>
2018-11-06 11:21:41 +0000

    fix/header-mobiles: header appearance on mobile: exit and menu show/hide TODO: set media order after main

--- app/components/site-header.js
+++ app/components/site-header.js
@@ -2,8 +2,21 @@ import Component from '@ember/component';
 import {action} from '@ember-decorators/object';
 
 export default class extends Component {
+  bShowSideMenu = false;
+
   @action
   onLogOutAction() {
     this.attrs.onLogOut && this.attrs.onLogOut();
   }
+
+  @action
+  showSideMenu() {
+    this.set('bShowSideMenu', true);
+  }
+
+  @action
+  hideSideMenu() {
+    this.set('bShowSideMenu', false);
+  }
+
 }
