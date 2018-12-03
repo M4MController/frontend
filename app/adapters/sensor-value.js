6aeb011d641018fab35dd7a27de51ad3fe7bc0a1
Ed Asriyan <ed-asriyan@protonmail.com>
2018-12-03 03:27:37 +0300

    Integrate with backend #73

--- app/adapters/sensor-value.js
+++ app/adapters/sensor-value.js
@@ -2,6 +2,9 @@ import ApplicationAdapter from './application';
 
 export default class extends ApplicationAdapter {
   query(store, type, query, recordArray) {
-    return this.ajax(`/sensor/${query.sensorId}/get_data`, 'GET');
+    return this.ajax(`/sensor/${query.sensorId}/get_data`, 'GET', {
+      from: query.from,
+      to: query.to,
+    });
   }
 }
