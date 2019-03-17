03f9a0884552169521560b743c822f32e53ecda6
Evgeniy Buevich <johnbuevich@yandex-team.ru>
2019-03-18 00:13:38 +0300

    todo

--- app/routes/account/company-account.js
+++ app/routes/account/company-account.js
@@ -4,7 +4,7 @@ export default class extends Route {
   model(params) {
     const companyId = params['company_id'];
 
-    const sensors = this.get('store').peekAll('sensor').filterBy('type', undefined); // удалить костыль после поддержки беком
+    const sensors = this.get('store').peekAll('sensor').filterBy('type', undefined); // TODO: удалить костыль после поддержки беком
 
     return {
       company: this.get('store').peekRecord('service-company', companyId),
