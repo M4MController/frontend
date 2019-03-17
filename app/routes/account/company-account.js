3423af2ef1d816c116627c4c138b2cb728df971e
Evgeniy Buevich <johnbuevich@yandex-team.ru>
2019-03-18 00:10:47 +0300

    table

--- app/routes/account/company-account.js
+++ app/routes/account/company-account.js
@@ -3,6 +3,12 @@ import Route from '@ember/routing/route';
 export default class extends Route {
   model(params) {
     const companyId = params['company_id'];
-    return this.get('store').peekRecord('service-company', companyId);
+
+    const sensors = this.get('store').peekAll('sensor').filterBy('type', undefined); // удалить костыль после поддержки беком
+
+    return {
+      company: this.get('store').peekRecord('service-company', companyId),
+      sensors,
+    };
   }
 }
