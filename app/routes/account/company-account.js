c3235887989082393f87e89be3a9e2b3f24b1bbb
Ed Asriyan <ed-asriyan@protonmail.com>
2019-03-19 00:26:17 +0300

    Update to the latest backend
    (#95)

--- app/routes/account/company-account.js
+++ app/routes/account/company-account.js
@@ -4,7 +4,8 @@ export default class extends Route {
   model(params) {
     const companyId = params['company_id'];
 
-    const sensors = this.get('store').peekAll('sensor').filterBy('type', undefined); // TODO: удалить костыль после поддержки беком
+    // todo: изменить на gps после того, как на бекенде будет разделение на типы сенсоров
+    const sensors = this.get('store').peekAll('sensor').filterBy('type', 0);
 
     return {
       company: this.get('store').peekRecord('service-company', companyId),
