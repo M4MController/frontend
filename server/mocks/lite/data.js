074b6f4d658311291c208de80bfd3dad2db9ee8c
Ed Asriyan <ed-asriyan@protonmail.com>
2019-04-27 02:46:31 +0300

    Increase auto update frequency in lite version #108

--- server/mocks/lite/data.js
+++ server/mocks/lite/data.js
@@ -1,28 +1,37 @@
 'use strict';
 
-const sensors = [
-  {
-    'id': 1,
-    'name': 'GPS',
-    'last_value': JSON.stringify({"lat": 1, "lon": 2}),
-    'status': 1,
-    'type': 0,
-  },
-  {
-    'id': 2,
-    'name': 'OBD',
-    'last_value': JSON.stringify({"some": 1, "values": 2}),
-    'status': 1,
-    'type': 0,
-  },
-];
+const carPosition = {lat: 55.751244, lon: 37.618423};
+
+const movePoint = function(point) {
+  const maxDelta = 0.005;
+  point.lat += (Math.random() - 0.5) * maxDelta;
+  point.lon += (Math.random() - 0.5) * maxDelta;
+};
 
 module.exports = function(app) {
   const express = require('express');
   let apiRouter = express.Router();
 
   apiRouter.get('/data', function(req, res) {
-    res.send(sensors);
+    // imitate car driving
+    movePoint(carPosition);
+
+    res.send([
+      {
+        'id': 1,
+        'name': 'GPS',
+        'last_value': JSON.stringify(carPosition),
+        'status': 1,
+        'type': 0,
+      },
+      {
+        'id': 2,
+        'name': 'OBD',
+        'last_value': JSON.stringify({'some': 1, 'values': 2}),
+        'status': 1,
+        'type': 0,
+      },
+    ]);
   });
 
   app.use('/api', apiRouter);
