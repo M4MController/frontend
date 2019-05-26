2b071b2e351bce996d53bcef1333b6de254447d6
Ed Asriyan <ed-asriyan@protonmail.com>
2019-05-26 03:35:27 +0300

    Install @ember/jquery

--- server/mocks/lite/sensor.js
+++ server/mocks/lite/sensor.js
@@ -2,12 +2,12 @@
 
 module.exports = function(app) {
   const express = require('express');
-  let apiRouter = express.Router();
+  const apiRouter = express.Router();
 
   apiRouter.get('/sensor/:sensor_id/data', function(req, res) {
     const maxDelta = 5;
     const data = [];
-    let date = new Date();
+    const date = new Date();
     for (let i = 0; i < 1000; ++i) {
       date.setMinutes(date.getMinutes() + 1);
       data.push({'date': date.toISOString(), 'value': {[req.query['field']]: Math.sin(i / 50) * (Math.random() - 0.5) * maxDelta}});
