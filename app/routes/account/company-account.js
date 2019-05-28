72585af9b0f0b6a5af291be5a8c040203cca16ac
Ed Asriyan <ed-asriyan@protonmail.com>
2019-05-28 23:59:01 +0300

    Create core/table-components

--- app/routes/account/company-account.js
+++ app/routes/account/company-account.js
@@ -4,11 +4,38 @@ export default class extends Route {
   model(params) {
     const companyId = params['company_id'];
 
-    // todo: изменить на gps после того, как на бекенде будет разделение на типы сенсоров
-    const sensors = this.get('store').peekAll('sensor').filterBy('type', 0);
+    const sensors = this.get('store').peekAll('sensor').filterBy('typeName', 'obd');
 
     return {
       company: this.get('store').peekRecord('service-company', companyId),
+      clients: [
+        {
+          status: '7/10',
+          model: 'Mercedes-Benz',
+          ownerName: 'Строганов Юрий Васильевич',
+          ownerPhone: '+7916826782',
+        }, {
+          status: '4/10',
+          model: 'Porshe 777',
+          ownerName: 'Куров Андрей Владимирович',
+          ownerPhone: '+74955553535',
+        }, {
+          status: '10/10',
+          model: 'BMW 7',
+          ownerName: 'Рудаков Игорь Владимирович',
+          ownerPhone: '+7999777777',
+        }, {
+          status: '2/10',
+          model: 'Lada Kalina',
+          ownerName: 'Рязанова Наталия Юрьевна',
+          ownerPhone: '+74957846248',
+        }, {
+          status: '9/10',
+          model: 'Mazda 3',
+          ownerName: 'Тихомирова Елизавета Алексеевна',
+          ownerPhone: '+74957846248',
+        },
+      ],
       sensors,
     };
   }
