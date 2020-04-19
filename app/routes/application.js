3c37fd1a81adae82f903ca7e308ae42d9da9a4ae
Ed Asriyan <ed-asriyan@protonmail.com>
2020-04-19 21:04:07 +0300

    Implement initial setup

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,12 +3,14 @@ import {inject as service} from '@ember/service';
 
 export default class extends Route {
   @service intl;
+  @service auth;
 
   beforeModel() {
     return this.intl.setLocale(['ru-ru']); /* array optional */
   }
 
   async model() {
+    await this.get('auth').init();
     return {
       user: await this.get('store').findRecord('user', 1).catch(() => null),
     };
