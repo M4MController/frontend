c99b48dbcb791de3d432328d1ad6ededcac48397
Evgeniy <johnbuevich@yandex-team.ru>
2020-05-07 01:39:07 +0300

    карта

--- app/components/sensor-gps-card.js
+++ app/components/sensor-gps-card.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import {action} from '@ember/object';
 import {on, observes} from '@ember-decorators/object';
+import {IS_LITE_MODE} from '../constants';
 
 export default class extends Component {
   zoom = 17;
@@ -19,7 +20,7 @@ export default class extends Component {
       this.set('lat', this.get('sensor.value.lat'));
       this.set('lng', this.get('sensor.value.lon'));
       this.set('hasMap', !(this.get('sensor.value.lon') === undefined || this.get('sensor.value.lat') === undefined));
-      this.set('hasData', !(this.get('sensor.value') === undefined));
+      this.set('hasData', !(this.get('sensor.value') === undefined) && IS_LITE_MODE);
     }
   }
 
