9bf58d4ab6ad6248d6cbe25de0bcdd0dcd8c970c
Ed Asriyan <ed-asriyan@protonmail.com>
2020-05-07 17:06:27 +0300

    Move default mode to new API

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,7 @@
 import DS from 'ember-data';
 import {inject as service} from '@ember/service';
 
-import {IS_LITE_MODE} from '../constants';
-
-const BaseAdapter = class extends DS.Adapter {
+export default class extends DS.Adapter {
   @service api;
 
   ajax(url, method, data) {
@@ -21,20 +19,4 @@ const BaseAdapter = class extends DS.Adapter {
   updateRecord(store, type, snapshot) {
     return this.ajax(this.buildUrlUpdateRecord(snapshot.id), 'PATCH', this.serialize(snapshot));
   }
-};
-
-const DefaultApplicationAdapter = class extends BaseAdapter {
-  buildUrlFindAll() {
-    return '/v2/user/relations';
-  }
-
-  buildUrlFindRecord() {
-    return '/v2/user/relations';
-  }
-};
-
-// адаптер пустой, тк он не должен вызываться в лайт-моде
-// (за исключением случая, когда дочерний адаптер переопределён методами)
-const LiteApplicationAdapter = BaseAdapter;
-
-export default IS_LITE_MODE ? LiteApplicationAdapter : DefaultApplicationAdapter;
+}
